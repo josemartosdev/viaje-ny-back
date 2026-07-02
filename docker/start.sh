7ed4bbb6640bc9bf424cd966c93b0b8c6b1730e6
Pepe MR <pepemartos98@gmail.com>
2026-07-02 14:32:18 +0200

    Ensure runtime .env fallback in Railway start script

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,10 @@ set -e
 
 PORT="${PORT:-8080}"
 
+if [ ! -f .env ]; then
+	printf "APP_ENV=%s\nAPP_DEBUG=%s\nAPP_SECRET=%s\n" "${APP_ENV:-prod}" "${APP_DEBUG:-0}" "${APP_SECRET:-change-me-in-railway}" > .env
+fi
+
 php bin/console cache:clear --env=prod --no-debug
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --env=prod
 
