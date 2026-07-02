6f8efe389709246ffd7f58b33737c206c2051a4f
Pepe MR <pepemartos98@gmail.com>
2026-07-02 14:21:37 +0200

    cambio produccion

--- docker/start.sh
+++ docker/start.sh
@@ -6,4 +6,4 @@ PORT="${PORT:-8080}"
 php bin/console cache:clear --env=prod --no-debug
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --env=prod
 
-exec php -S 0.0.0.0:${PORT} -t public
+exec php -S 0.0.0.0:${PORT} -t public public/index.php
